7f64d09b85701793feb44d7ae857787bddc7ff60
mahmoudzakaria12 <mahmoudzsali247@gmail.com>
2025-06-07 19:52:12 +0300

    Add built index.css file

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./index-DMK-F0rV.css";
 import ContextProvider from "./context/context.provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
